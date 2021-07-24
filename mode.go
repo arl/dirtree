a50ee1af42b133c11160a2dadc640cf108faae23
Aurélien Rainone <aurelien.rainone@gmail.com>
2021-07-24 11:02:13 +0200

    Add mode(_test).go

--- mode.go
+++ mode.go
@@ -2,9 +2,12 @@ package dirtree
 
 import (
 	"fmt"
+	"hash/crc32"
+	"io"
 	"io/fs"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
 
@@ -14,29 +17,33 @@ import (
 type PrintMode uint32
 
 const (
-	// ModeType indicates if file is a directory, a file or something else.
-	// It prints "t=dir", "t=file" or "t=other".
+	// ModeType indicates if file is a directory, a regular file or something
+	// else. It prints "typ=d", "typ=f" or "typ=?" respectively.
 	ModeType PrintMode = 1 << iota
 
 	// ModeSize reports the length in bytes for regular files. For other types
-	// it shows NA (not applicable) since the size would be system dependent.
-	// It prints "s=1234" or "s=NA".
+	// it shows n/a (not applicable) since the size would be system dependent.
+	// It prints "s=     1234" or "s=n/a".
 	ModeSize
 
 	// ModeSymlink indicates if a file is a symlink.
-	// It prints "sym=true" or "sym=false".
+	// It prints "sym=1" or "sym=0".
 	ModeSymlink
 
-	// ModePerm reports unix permission bits.
-	// It prints "perm=o644" for example.
+	// ModeCRC32 computes and reports the CRC-32 checksum for regular files. For
+	// other file types, or for files which permissions prevent reading, it
+	// shows n/a (i.e. not applicable). Example "crc=294a245b" or "crc=n/a"
+	ModeCRC32
+
+	// ModePerm shows the Unix permission bits, in octal. Example "perm=644".
 	ModePerm
 
-	// ModeAll is a mask showing all information aout a file.
-	ModeAll PrintMode = ModeType | ModeSize | ModeSymlink | ModePerm
-
-	// ModeStd is a mask showing kind and size all standard information aout a file. Should be
-	// enough in most cases.
+	// ModeStd is a mask showing kind and size all standard information aout a
+	// file. Should be enough in most cases.
 	ModeStd PrintMode = ModeType | ModeSize
+
+	// ModeAll is a mask showing all information aout a file.
+	ModeAll PrintMode = ModeType | ModeSize | ModeSymlink | ModePerm | ModeCRC32
 )
 
 type ftype int
@@ -61,14 +68,65 @@ func filetype(dirent fs.DirEntry) ftype {
 func (t ftype) String() string {
 	switch t {
 	case typeDir:
-		return "dir"
+		return "d"
 	case typeFile:
-		return "file"
+		return "f"
 	case typeOther:
-		return "other"
+		return "?"
 	}
 
 	panic(fmt.Sprintf("invalid filetype ftype(%d)", t))
+}
+
+const notApplicable = "n/a"
+
+// we pad the size to sizeDigits, with spaces, so that for most filenames all
+// the fields are aligned. We're hoewever not going to truncate the size of bigger files
+// just to respect that rule, we're making an exception in those cases.
+const sizeDigits = 9
+
+func formatSize(size int64) string {
+	str := fmt.Sprintf("%d", size)
+	if len(str) > sizeDigits {
+		return str
+	}
+	return fmt.Sprintf("%-*d", sizeDigits, size)
+}
+
+func sizeNA() string {
+	return fmt.Sprintf("%-*s", sizeDigits, notApplicable)
+}
+
+// buffer used in io.CopyBUffer to reduce allocations
+// while calculating the file checksum.
+var iobuf [32 * 1024]byte
+
+// number of chars in hexadecimal representation of a CRC-32.
+const crcChars = crc32.Size * 2 // 2 since 2 chars per raw byte
+
+func checksum(ft ftype, path string) (chksum string) {
+	defer func() {
+		if e := recover(); e != nil || chksum == "" {
+			chksum = fmt.Sprintf("%-*s", crcChars, notApplicable)
+		}
+	}()
+
+	if ft != typeFile {
+		return
+	}
+
+	h := crc32.NewIEEE()
+	f, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	if _, err := io.CopyBuffer(h, f, iobuf[:]); err != nil {
+		panic(err)
+	}
+
+	chksum = fmt.Sprintf("%0*x", crcChars, h.Sum32())
+	return
 }
 
 // format prints the name
@@ -79,43 +137,73 @@ func (mode PrintMode) format(root, fullpath string, dirent fs.DirEntry) (format 
 		}
 	}()
 
-	var sb strings.Builder
-	rel, err := filepath.Rel(root, fullpath)
-	if err != nil {
-		return "", fmt.Errorf("can't find relative path: %s", err)
+	sb := strings.Builder{}
+	ft := filetype(dirent)
+
+	sep := func() {
+		// Add separator between mode expressions if necessary
+		if sb.Len() != 0 {
+			sb.WriteByte(' ')
+		}
 	}
 
-	var fi fs.FileInfo // lazy
+	var fi fs.FileInfo // lazily created
 	stat := func() fs.FileInfo {
 		if fi != nil {
 			return fi
 		}
 		fi, err = os.Lstat(fullpath)
 		if err != nil {
-			panic(fmt.Errorf("can't get size: %v", err))
+			panic(fmt.Errorf("lstat failed: %v", err))
 		}
 		return fi
 	}
 
-	ft := filetype(dirent)
-
-	sb.WriteString(rel)
 	if mode&ModeType != 0 {
-		sb.WriteByte(' ')
+		sep()
 		sb.WriteString("t=")
 		sb.WriteString(ft.String())
 	}
 
+	if mode&ModePerm != 0 {
+		sep()
+		sb.WriteString("perm=")
+		sb.WriteString(strconv.FormatUint(uint64(stat().Mode()&fs.ModePerm), 8))
+	}
+
+	if mode&ModeSymlink != 0 {
+		sep()
+		issym := byte('0')
+		if (stat().Mode() & fs.ModeSymlink) != 0 {
+			issym = '1'
+		}
+		sb.WriteString("sym=")
+		sb.WriteByte(issym)
+	}
+
 	if mode&ModeSize != 0 {
-		sb.WriteByte(' ')
-		sb.WriteString("s=")
+		sep()
+		sb.WriteString("sz=")
 		switch ft {
 		case typeFile:
-			fmt.Fprintf(&sb, "%d", stat().Size())
+			sb.WriteString(formatSize(stat().Size()))
 		default:
-			sb.WriteByte('0')
+			sb.WriteString(sizeNA())
 		}
 	}
+
+	if mode&ModeCRC32 != 0 {
+		sep()
+		sb.WriteString("crc=")
+		sb.WriteString(checksum(ft, fullpath))
+	}
+
+	sep()
+	rel, err := filepath.Rel(root, fullpath)
+	if err != nil {
+		return "", fmt.Errorf("can't find relative path: %s", err)
+	}
+	sb.WriteString(rel)
 
 	return sb.String(), err
 }
--- mode_test.go
+++ mode_test.go
@@ -1,1 +1,140 @@
 package dirtree
+
+import (
+	"io/fs"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+type dentry struct {
+	info fs.FileInfo
+}
+
+func newDentry(path string) *dentry {
+	info, err := os.Lstat(path)
+	if err != nil {
+		panic(err)
+	}
+	return &dentry{info}
+}
+
+func (d *dentry) Name() string               { return d.info.Name() }
+func (d *dentry) IsDir() bool                { return d.info.IsDir() }
+func (d *dentry) Type() fs.FileMode          { return d.info.Mode().Type() }
+func (d *dentry) Info() (fs.FileInfo, error) { return d.info, nil }
+
+func TestPrintMode_format(t *testing.T) {
+	root := t.TempDir()
+
+	// Create a directory structure rooted at 'root'.
+	// TODO(arl) add structure with 'tree' output
+
+	var (
+		dirA     = filepath.Join(root, "A")
+		file1    = filepath.Join(root, "A", "file1")
+		symfile1 = filepath.Join(root, "A", "symfile1")
+		symdirA  = filepath.Join(root, "A", "B", "symdirA")
+	)
+	if err := os.MkdirAll(filepath.Join(dirA, "B"), os.ModePerm); err != nil {
+		t.Fatal(err)
+	}
+	if err := os.WriteFile(file1, []byte("dummy content"), os.ModePerm); err != nil {
+		t.Fatal(err)
+	}
+	if err := os.Symlink(file1, symfile1); err != nil {
+		t.Fatal(err)
+	}
+	if err := os.Symlink(dirA, symdirA); err != nil {
+		t.Fatal(err)
+	}
+
+	tests := []struct {
+		name       string
+		mode       PrintMode
+		root       string
+		fullpath   string
+		dirent     fs.DirEntry
+		wantFormat string
+		wantErr    bool
+	}{
+		{
+			name: "mode=ModeType/file1",
+			mode: ModeType,
+			root: root, fullpath: file1, dirent: newDentry(file1),
+			wantFormat: "t=f A/file1",
+		},
+		{
+			name: "mode=ModeSize/file1",
+			mode: ModeSize,
+			root: root, fullpath: file1, dirent: newDentry(file1),
+			wantFormat: "sz=13        A/file1",
+		},
+		{
+			name: "mode=ModeStd/file1",
+			mode: ModeStd,
+			root: root, fullpath: file1, dirent: newDentry(file1),
+			wantFormat: "t=f sz=13        A/file1",
+		},
+		{
+			name: "mode=ModeAll/file1",
+			mode: ModeAll,
+			root: root, fullpath: file1, dirent: newDentry(file1),
+			wantFormat: "t=f perm=775 sym=0 sz=13        crc=0451ac5e A/file1",
+		},
+		{
+			name: "mode=ModeStd+ModeSymlink/dirA",
+			mode: ModeStd | ModeSymlink,
+			root: root, fullpath: dirA, dirent: newDentry(dirA),
+			wantFormat: "t=d sym=0 sz=n/a       A",
+		},
+		{
+			name: "mode=ModeType+ModeSymlink/symfile1",
+			mode: ModeStd | ModeSymlink,
+			root: root, fullpath: symfile1, dirent: newDentry(symfile1),
+			wantFormat: "t=? sym=1 sz=n/a       A/symfile1",
+		},
+		{
+			name: "mode=ModeType+ModeSymlink/symdirA",
+			mode: ModeStd | ModeSymlink,
+			root: root, fullpath: symdirA, dirent: newDentry(symdirA),
+			wantFormat: "t=? sym=1 sz=n/a       A/B/symdirA",
+		},
+		{
+			name: "mode=ModeCRC32/file1",
+			mode: ModeCRC32,
+			root: root, fullpath: file1, dirent: newDentry(file1),
+			wantFormat: "crc=0451ac5e A/file1",
+		},
+		{
+			name: "mode=ModeCRC32/dirA",
+			mode: ModeCRC32,
+			root: root, fullpath: dirA, dirent: newDentry(dirA),
+			wantFormat: "crc=n/a      A",
+		},
+		{
+			name: "mode=ModeCRC32/symfile1",
+			mode: ModeCRC32,
+			root: root, fullpath: symfile1, dirent: newDentry(symfile1),
+			wantFormat: "crc=n/a      A/symfile1",
+		},
+		{
+			name: "mode=ModePerm/symdirA",
+			mode: ModePerm,
+			root: root, fullpath: symdirA, dirent: newDentry(symdirA),
+			wantFormat: "perm=777 A/B/symdirA",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.mode.format(tt.root, tt.fullpath, tt.dirent)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("PrintMode.format() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.wantFormat {
+				t.Errorf("format error\ngot :%q\nwant:%q", got, tt.wantFormat)
+			}
+		})
+	}
+}
