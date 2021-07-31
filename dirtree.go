7c41efa37e8ed257acf8c34a1dc9232b26852cde
Aurélien Rainone <aurelien.rainone@gmail.com>
2021-07-31 23:35:03 +0200

    Implement writer and walk

--- dirtree.go
+++ dirtree.go
@@ -10,76 +10,96 @@ import (
 	"strings"
 )
 
-// Write prints the directory tree rooted at root in w.
-func Write(w io.Writer, root string, opts ...Option) error {
+type writer struct {
+	cfg      config
+	seenRoot bool
+	root     string
+	bufw     *bufio.Writer
+}
+
+func newWriter(w io.Writer, root string, opts ...Option) (*writer, error) {
 	cfg := defaultCfg
 	for _, o := range opts {
 		if err := o.apply(&cfg); err != nil {
-			return fmt.Errorf("dirtree: configuration error: %v", err)
+			return nil, fmt.Errorf("dirtree: configuration error: %v", err)
 		}
 	}
 
-	seenRoot := false
-	bufw := bufio.NewWriter(w)
-	err := filepath.WalkDir(root, func(fullpath string, dirent fs.DirEntry, err error) error {
-		if err != nil {
+	return &writer{
+		bufw:     bufio.NewWriter(w),
+		seenRoot: false,
+		root:     root,
+		cfg:      cfg,
+	}, nil
+}
+
+func (w *writer) walk(fullpath string, dirent fs.DirEntry, err error) error {
+	if err != nil {
+		return err
+	}
+
+	// Exclude root
+	if !w.seenRoot {
+		w.seenRoot = true
+		if !w.cfg.showRoot {
+			return nil
+		}
+	}
+
+	// Path conversion: relative to root and slash based
+	rel, err := filepath.Rel(w.root, fullpath)
+	if err != nil {
+		return err
+	}
+
+	// Depth check
+	if w.cfg.depth != 0 {
+		if len(strings.Split(rel, string(os.PathSeparator))) > w.cfg.depth {
+			if dirent.IsDir() {
+				err = fs.SkipDir
+			}
 			return err
 		}
+	}
 
-		// Exclude root
-		if !seenRoot {
-			seenRoot = true
-			if !cfg.showRoot {
+	rel = filepath.ToSlash(rel)
+
+	// Ignore patterns
+	if w.cfg.ignore != nil {
+		for _, pattern := range w.cfg.ignore {
+			if m, _ := filepath.Match(pattern, rel); m {
 				return nil
 			}
 		}
+	}
 
-		// Path conversion: relative to root and slash based
-		rel, err := filepath.Rel(root, fullpath)
-		if err != nil {
-			return err
-		}
-
-		// Depth check
-		if cfg.depth != 0 {
-			if len(strings.Split(rel, string(os.PathSeparator))) > cfg.depth {
-				if dirent.IsDir() {
-					err = fs.SkipDir
-				}
-				return err
-			}
-		}
-
-		rel = filepath.ToSlash(rel)
-
-		// Ignore patterns
-		if cfg.ignore != nil {
-			for _, pattern := range cfg.ignore {
-				if m, _ := filepath.Match(pattern, rel); m {
-					return nil
-				}
-			}
-		}
-
-		line, err := cfg.mode.format(root, fullpath, dirent)
-		if err != nil {
-			return fmt.Errorf("can't format %s: %s", fullpath, err)
-		}
-		if _, err = bufw.WriteString(line); err != nil {
-			return err
-		}
-
-		// Write path
-		if _, err = bufw.WriteString(rel); err != nil {
-			return err
-		}
-		return bufw.WriteByte('\n')
-	})
+	line, err := w.cfg.mode.format(w.root, fullpath, dirent)
 	if err != nil {
+		return fmt.Errorf("can't format %s: %s", fullpath, err)
+	}
+	if _, err = w.bufw.WriteString(line); err != nil {
+		return err
+	}
+
+	// Write path
+	if _, err = w.bufw.WriteString(rel); err != nil {
+		return err
+	}
+	return w.bufw.WriteByte('\n')
+}
+
+// Write prints the directory tree rooted at root in w.
+func Write(w io.Writer, root string, opts ...Option) error {
+	dtw, err := newWriter(w, root, opts...)
+	if err != nil {
+		return err
+	}
+
+	if err := filepath.WalkDir(root, dtw.walk); err != nil {
 		return fmt.Errorf("dirtree: error walking directory: %v", err)
 	}
 
-	if err := bufw.Flush(); err != nil {
+	if err := dtw.bufw.Flush(); err != nil {
 		return fmt.Errorf("dirtree: can't write output: %s", err)
 	}
 	return nil
